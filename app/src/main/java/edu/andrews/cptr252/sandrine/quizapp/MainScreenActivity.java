d74347bd749da1fd54f0102ae4b26c8d40c9cb86
sandrine <sandrine@andrews.edu>
2022-04-07 20:38:01 -0400

    initial commit - created Main Screen Activity

---
+++ app/src/main/java/edu/andrews/cptr252/sandrine/quizapp/MainScreenActivity.java
@@ -0,0 +1,14 @@
+package edu.andrews.cptr252.sandrine.quizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
