d38252602b82c18c9751e68b15efe3aa52d0a7e7
sandrine <sandrine@andrews.edu>
2022-04-18 17:05:22 -0400

    added model layer for Question List

--- app/src/main/java/edu/andrews/cptr252/sandrine/quizapp/QuestionList.java
+++ app/src/main/java/edu/andrews/cptr252/sandrine/quizapp/QuestionList.java
@@ -11,4 +11,40 @@ public class QuestionList {
     private static QuestionList sOurInstance;
 
     /** List of Questions */
+    private ArrayList<Question> mQuestions;
+
+    private Context mAppContext;
+
+    /** constructor */
+    private QuestionList(Context appContext) {
+        mAppContext = appContext;
+        mQuestions = new ArrayList<>();
+        // for now, create a list of 10 dummy questions
+        for (int i = 0; i < 10; i++) {
+            Question question = new Question();
+            question.setQuestion("Question #" + i);
+            // every other answer is true
+            question.setAnswer(i % 2 == 0);
+            mQuestions.add(question);
+        }
+    }
+
+    /**
+     * Return the one and only instance of questions list.
+     * Create instance if it does not exist
+     * @param c - Application context
+     * @return reference to question list
+     */
+    public static QuestionList getInstance(Context c) {
+        if (sOurInstance == null) {
+            sOurInstance = new QuestionList(c.getApplicationContext());
+        }
+        return sOurInstance;
+    }
+
+    /**
+     * Return list of Questions
+     * @return Array of question objects
+     */
+    public ArrayList<Question> getQuestions() { return mQuestions; }
 }
